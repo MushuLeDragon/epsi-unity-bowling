5029b39d0d2545230fda66a7e0a4647a6a85a04d
MushuLeDragon <dragon.mushu@hotmail.com>
2019-02-08 14:20:04 +0100

    First commit

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject Ball;
+    private float offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position.z - Ball.transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, Ball.transform.position.z + offset);  
+    }
+}
